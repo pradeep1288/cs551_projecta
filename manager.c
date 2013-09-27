eb747e81d32492903d6332518928043667a2f525
Pradeep Nayak <pradeep1288@gmail.com>
2013-09-27 01:57:27 -0700

    1. rand port no, read config file

--- manager.c
+++ manager.c
@@ -1,6 +1,69 @@
 #include "manager.h"
 
-int main()
-{
+typedef struct config_file_info {
+    int stage_no;
+    int num_of_nodes;
+    long noonce;
+}Conf;
+char buf[1024];
 
+uint16_t getRandomPort()
+{
+    struct   sockaddr_in sin;
+    int addrlen, sd_server;
+    addrlen=sizeof(sin);
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr=inet_addr("127.0.0.1");
+    sin.sin_port=htons(0);
+    sd_server = socket(PF_INET, SOCK_STREAM, 0);
+    bind(sd_server, (struct sockaddr *) &sin, sizeof(sin));
+    getsockname(sd_server,(struct sockaddr*)&sin,&addrlen);
+    return ntohs(sin.sin_port); 
 }
+
+Conf* read_config(char* path)
+{
+    Conf *conf = (Conf* )malloc(sizeof(Conf));
+    FILE *fp;
+    fp = fopen(path, "r");
+    char temp[1024];
+    int no_of_lines = 0;
+    while (fp!=NULL)
+    {
+        fgets(buf, sizeof(buf), fp);
+        if (buf[0]== '#')
+        {
+            no_of_lines++;
+            continue;
+        }
+        else if (no_of_lines == 1)
+        {
+            sscanf(buf, "%s %d", temp, &(conf->stage_no));
+            no_of_lines++;
+        }        
+        else if (no_of_lines == 2)
+        {
+            sscanf(buf, "%s %d",temp , &(conf->num_of_nodes));
+            no_of_lines++;
+        }
+        else if (no_of_lines == 3)
+        {
+            sscanf(buf, "%s %ld", temp, &(conf->noonce));
+            no_of_lines++;
+        }
+        else 
+        {
+            break;
+        }
+    }
+    return conf;
+}
+
+int main(int argc, char const *argv[])
+{
+    Conf *conf;
+    conf = read_config("foo.txt");
+    printf("%d\n",getRandomPort());
+    return 0;
+}        
